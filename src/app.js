6e97ae46f2d4e099c120fe2b429db67d87a892fe
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2023-02-15 19:30:22 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -4,17 +4,14 @@ const express = require('express')
 // instanciando o servidor
 const app = express()
 
-
 // configurando o servidor para receber requisições com o corpo no formato JSON
 app.use(express.json())
 
-
 // importando os controllers
-const usuarioController = require('./controllers/usuario-controller.js')
-const tarefaController = require('./controllers/tarefa-controller.js')
+const usuarioController = require('./controllers/usuarioController.js')
+const tarefaController = require('./controllers/tarefaController.js')
 
 usuarioController.rotas(app)
 tarefaController.rotas(app)
-
 
 module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 const app = require('./app.js')
 
-
 // escolhendo a porta em que o servidor será aberto
 const port = 3000
-
 
 // abrindo o servidor na porta escolhida
 app.listen(port, () => {
